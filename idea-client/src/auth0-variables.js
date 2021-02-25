cb38330c22f8c19d2614dc47de3cbad06b8e57b5
luqmanomar <50601695+luqmanomar@users.noreply.github.com>
2021-02-25 10:48:58 +0800

    Create auth0-variables.js

---
+++ idea-client/src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: "dev-ivynzgje.us.auth0.com",
+    roleUrl: "https://idea-generation-manager/role",
+    clientId: "R3PuUJroIkYucsIGmy13TqVDvrYRIGu6",
+    callbackUrl: "http://localhost:3000/callback"
+  };
