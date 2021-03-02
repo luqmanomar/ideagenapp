0c6f17838d12e57df6a9069b7a6dd2f7fbae21af
luqmanomar <50601695+luqmanomar@users.noreply.github.com>
2021-03-02 09:25:38 +0800

    Update dashboard.js

--- idea-client/src/pages/dashboard.js
+++ idea-client/src/pages/dashboard.js
@@ -3,10 +3,11 @@ import { Redirect } from "react-router-dom";
 
 import { AuthConsumer } from "../authContext";
 import Can from "../components/Can";
-import Logout from "../components/Logout";
-import Profile from "../components/Profile";
-import PostsList from "../components/PostsList";
+// import Logout from "../components/Logout";
+// import Profile from "../components/Profile";
+import EventsList from "../components/EventsList";
 import {NavBar} from "../components/NavBar";
+import IdeasList from "../components/IdeasList";
 // import { Link } from "react-router-dom";
 
 const DashboardPage = () => (
@@ -21,12 +22,16 @@ const DashboardPage = () => (
             <br></br>
 
             <h2>Welcome back</h2>
+            <p>{user.email}</p>
             {/* <Link to={"/profile"}><button> Profile </button></Link>
             <Link to={"/ideas"}><button> Idea </button></Link> */}
-            <Logout />
+            {/* <Logout /> */}
+            {/* <Link to={"/addprofile"}><button className="badge badge-success"> + Create profile </button></Link> */}
+
             <br />
-            <Profile />
-            <PostsList />
+            {/* <Profile /> */}
+            <EventsList />
+            <IdeasList />
           </div>
         )}
         no={() => <Redirect to="/" />}
